9550da336e766511c1282588bd1fd37bfc14cab9
wschimitsch <wfs716@gmail.com>
2026-06-12 21:01:04 -0400

    feat: add js/css

---
+++ assets/animation.js
@@ -0,0 +1,12 @@
+const images = document.querySelectorAll('.img-fade img, .text-fade');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    }
+  });
+}, { threshold: 0.1 });
+
+images.forEach(img => observer.observe(img));
